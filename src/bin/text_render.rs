1850e95f13e7d8fa1c6ba1a30ef68eb171397f6f
koatl <koatliaxl@gmail.com>
2025-05-14 13:23:04 +0300

    text render test (ASKII): code cleanup

--- src/bin/text_render.rs
+++ src/bin/text_render.rs
@@ -1,9 +1,8 @@
 use freetype as ft;
+use std::io::stdin;
 
 //const WIDTH: i32 = 48 * 1;
-const HEIGHT: i32 = 32 * 1;
-
-//type Figure = [[u8; WIDTH as usize]; HEIGHT as usize];
+//const HEIGHT: i32 = 32 * 1;
 
 fn main() {
     let library = ft::Library::init().unwrap();
@@ -11,7 +10,7 @@ fn main() {
 
     println!("Enter symbol to draw and/or size multiplier:");
     let mut input = String::new();
-    std::io::stdin()
+    stdin()
         .read_line(&mut input)
         .expect("Error while trying read input");
     let mut size = 1;
@@ -24,10 +23,6 @@ fn main() {
                     Some(' ') => {
                         // split at 2 and not at 1 to omit preceding whitespace
                         let (_, second) = input.split_at(2);
-                        /*let b = second.as_bytes();
-                        for n in b {
-                            print!("{} ", n)
-                        }*/
                         if let Some(s) = second.strip_suffix("\r\n") {
                             if let Ok(multiplier) = s.parse::<u32>() {
                                 size = multiplier;
@@ -44,47 +39,40 @@ fn main() {
                 face.load_char(ch as usize, ft::face::LoadFlag::RENDER)
                     .unwrap();
             }
-            //'\n' => println!("empty input"),
+            '\n' | '\r' => (),
             _ => println!("Please, enter a printable character"),
         }
-    } else {
-        //println!("empty input")
     }
 
     let glyph = face.glyph();
-    let x = glyph.bitmap_left() + 5;
-    let y = HEIGHT - glyph.bitmap_top() - 8 * size as i32;
+    /*let x = glyph.bitmap_left() + 5;
+    let y = HEIGHT - glyph.bitmap_top() - 8 * size as i32;*/
 
-    let figure = draw_bitmap(glyph.bitmap(), x, y);
+    let figure = draw_bitmap(glyph.bitmap() /*, x, y*/);
     let mapping = b" .:-;+*x#@";
     let mapping_scale = (mapping.len() - 1) as f32;
-    for col in 0..figure[0].len() {
-        for j in 0..figure.len() {
-            let v = figure[j][col];
+    for i in 0..figure.len() {
+        for j in 0..figure[i].len() {
+            let v = figure[i][j];
             let i = ((v as f32 / 256.0) * mapping_scale + 0.5) as usize;
             // '$' in the output if something wrong
             let c = mapping.get(i).cloned().unwrap_or(b'$') as char;
             print!("{}", c)
         }
-        println!(/*"x is: {} ", x*/)
+        println!()
     }
 }
 
-fn draw_bitmap(bitmap: ft::Bitmap, x: i32, y: i32) -> Vec<Vec<u8>> {
-    //let mut figure = [[0; WIDTH as usize]; HEIGHT as usize];
+fn draw_bitmap(bitmap: ft::Bitmap /*, _x: i32, _y: i32*/) -> Vec<Vec<u8>> {
     let mut figure = Vec::new();
     let w = (bitmap.width() * 2) as usize;
-    let x_max = x + w as i32;
-    let y_max = y + bitmap.rows();
-    for (p, _i) in (x..x_max).enumerate() {
+    //let x_max = w;
+    //let y_max = bitmap.rows() as usize;
+    for i in 0..bitmap.rows() as usize {
         let mut row = Vec::new();
-        for (q, _j) in (y..y_max).enumerate() {
-            /*if i < 0 || j < 0 || i >= WIDTH || j >= HEIGHT {
-                continue;
-            }*/
-            let v = bitmap.buffer()[(q * w + p) / 2];
+        for j in 0..w {
+            let v = bitmap.buffer()[(i * w + j) / 2];
             row.push(v);
-            //figure[j as usize][i as usize]
         }
         figure.push(row)
     }
