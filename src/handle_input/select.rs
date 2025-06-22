dddfc7396d552ac4b48fc7b5dc8369eec4709204
Koatl <koatliaxl@gmail.com>
2025-06-22 17:38:35 +0300

    practical usage of text rendering

--- src/handle_input/select.rs
+++ src/handle_input/select.rs
@@ -1,7 +1,8 @@
 use crate::draw::BODY_GFX_SCALE;
 use crate::{State, World};
 use mat_vec::{Matrix4x4, Vector3, Vector4};
-use n_body_sim::gui::{Label, RootGIE, GIE};
+use n_body_sim::gui::{Label, RootGIE/*, GIE*/};
+use n_body_sim::Body;
 
 pub fn select_obj(state: &mut State, world: &World, window_size: (i32, i32), gui: &mut RootGIE) {
     state.selected = -1;
@@ -9,11 +10,52 @@ pub fn select_obj(state: &mut State, world: &World, window_size: (i32, i32), gui
         state.last_cursor_pos.0 as f32,
         state.last_cursor_pos.1 as f32,
     );
-    let view_mat = Matrix4x4::<f32>::new_LookAt_matrix(
+    /*let view_mat = Matrix4x4::<f32>::new_LookAt_matrix(
         state.view_pos,
         Vector3::new(0.0, 0.0, -1.0),
         Vector3::new(0.0, 1.0, 0.0),
-    );
+    );*/
+    let (w, h) = (window_size.0 as f32, window_size.1 as f32);
+    //let ratio = w / h;
+    /*let proj_mat = Matrix4x4::<f32>::new_orthographic_projection(
+        state.view_scale * ratio,
+        state.view_scale,
+        10.0,
+        0.1,
+    );*/
+    let (proj_mat, view_mat) = calc_matrices(state, window_size);
+
+    for body in world
+        .bodies
+        .lock()
+        .expect("Main: failed to acquire lock for selection")
+        .iter()
+    {
+        let pos_on_scr = calc_body_pos_on_screen(window_size, &view_mat, &proj_mat, body);
+        /*let (ox, oy, _) = body.pos.get_components();
+        let pos_vec4 = Vector4::new(ox as f32, oy as f32, 0.0, 1.0);
+        //todo Vec4: new from Vec3
+        //let pos_vec4 = Vector4::from(o.pos);
+        let mut pos_on_scr = proj_mat.clone() * view_mat.clone() * pos_vec4;
+        pos_on_scr.set_x((pos_on_scr.x() + 1.0) * 0.5 * w);
+        pos_on_scr.set_y((1.0 - pos_on_scr.y()) * 0.5 * h);*/
+
+        let equation_val = (x - pos_on_scr.x()).powi(2) + (y - pos_on_scr.y()).powi(2);
+        let radius = BODY_GFX_SCALE * body.get_radius() as f32;
+        if equation_val < (radius / state.view_scale * (w + h) / 2.0).powi(2) {
+            state.selected = body.get_id() as i64;
+            //println!("mass: {}", o.mass);
+            //println!("{}, {}", pos_on_scr.x(), pos_on_scr.y());
+            //let world_pos = o.pos;
+            update_coord_label(gui, "body_pos_text", pos_on_scr, window_size, body)
+        }
+    }
+}
+
+pub fn calc_matrices(
+    state: &mut State,
+    window_size: (i32, i32),
+) -> (Matrix4x4<f32>, Matrix4x4<f32>) {
     let (w, h) = (window_size.0 as f32, window_size.1 as f32);
     let ratio = w / h;
     let proj_mat = Matrix4x4::<f32>::new_orthographic_projection(
@@ -22,32 +64,47 @@ pub fn select_obj(state: &mut State, world: &World, window_size: (i32, i32), gui
         10.0,
         0.1,
     );
-    for o in world
-        .bodies
-        .lock()
-        .expect("Main: failed to acquire lock for selection")
-        .iter()
-    {
-        let (ox, oy, _) = o.pos.get_components();
-        let pos_vec4 = Vector4::new(ox as f32, oy as f32, 0.0, 1.0);
-        //todo Vec4: new from Vec3
-        //let pos_vec4 = Vector4::from(o.pos);
-        let mut pos_on_scr = proj_mat.clone() * view_mat.clone() * pos_vec4;
-        pos_on_scr.set_x((pos_on_scr.x() + 1.0) * 0.5 * w);
-        pos_on_scr.set_y((1.0 - pos_on_scr.y()) * 0.5 * h);
-        let equation_val = (x - pos_on_scr.x()).powi(2) + (y - pos_on_scr.y()).powi(2);
-        let radius = BODY_GFX_SCALE * o.get_radius() as f32;
-        if equation_val < (radius / state.view_scale * (w + h) / 2.0).powi(2) {
-            state.selected = o.get_id() as i64;
-            println!("mass: {}", o.mass);
-            let world_pos = o.pos;
-            let mut pos_label/*: &mut Label*/ = gui.get_gie("body_pos_text").unwrap();
-            pos_label.change_pos((pos_on_scr.x() as i32, pos_on_scr.y() as i32));
-            pos_label
-                .get_type()
-                .downcast_mut::<Label>()
-                .expect("failed to downcast GIE as Label")
-                .change_text(format!("{}, {}", world_pos.x(), world_pos.y()));
-        }
-    }
+    let view_mat = Matrix4x4::<f32>::new_LookAt_matrix(
+        state.view_pos,
+        Vector3::new(0.0, 0.0, -1.0),
+        Vector3::new(0.0, 1.0, 0.0),
+    );
+    (proj_mat, view_mat)
+}
+
+pub fn calc_body_pos_on_screen(
+    window_size: (i32, i32),
+    view_mat: &Matrix4x4<f32>,
+    proj_mat: &Matrix4x4<f32>,
+    body: &Body,
+) -> Vector4<f32> {
+    let (w, h) = (window_size.0 as f32, window_size.1 as f32);
+    let (ox, oy, _) = body.pos.get_components();
+    let pos_vec4 = Vector4::new(ox as f32, oy as f32, 0.0, 1.0);
+    //todo Vec4: new from Vec3
+    //let pos_vec4 = Vector4::from(o.pos);
+    let mut pos_on_scr = proj_mat.clone() * view_mat.clone() * pos_vec4;
+    pos_on_scr.set_x((pos_on_scr.x() + 1.0) * 0.5 * w);
+    pos_on_scr.set_y((1.0 - pos_on_scr.y()) * 0.5 * h);
+    pos_on_scr
+}
+
+pub fn update_coord_label(
+    gui: &mut RootGIE,
+    label_name: &str,
+    pos_on_scr: Vector4<f32>,
+    window_size: (i32, i32),
+    body: &Body,
+) {
+    let (w, h) = (window_size.0 as f32, window_size.1 as f32);
+    let pos_label/*: &mut Label*/ = gui.get_gie(label_name).unwrap();
+    pos_label.change_pos((
+        (pos_on_scr.x() - w / 2.0) as i32,
+        (-pos_on_scr.y() + h / 2.0) as i32,
+    ));
+    pos_label
+        .get_type()
+        .downcast_mut::<Label>()
+        .expect("failed to downcast GIE as Label")
+        .change_text(format!("{:.2}, {:.2}", body.pos.x(), body.pos.y()));
 }
--- src/init/gui.rs
+++ src/init/gui.rs
@@ -3,7 +3,7 @@ use n_body_sim::gui::{Label, RootGIE};
 pub fn init_gui() -> RootGIE {
     let mut gui = RootGIE::new();
     let text_draw = crate::draw::draw_text;
-    let body_pos = Label::new((0, 0), "body_pos_text".to_string(), "", 1.0, text_draw);
+    let body_pos = Label::new((0, 0), "body_pos_text".to_string(), "", 0.5, text_draw);
     gui.add_gie(body_pos);
     gui
 }
